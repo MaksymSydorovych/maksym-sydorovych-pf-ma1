7a6522a677990303f4127d554c8c11266616f7e7
MaksymSydorovych <71548115+MaksymSydorovych@users.noreply.github.com>
2020-12-06 21:31:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+//question 1 Declare a variable called firstVar but don't initialise it with a value.
+//answer 1
+var fitsVar;
+//question 2 Declare a variable called name and assign it your first name.
+//answer 2
+var name = maksym;
+//question 3 Declare and initialise a variable with a number value.
+//answer 3
+var price = 25;
+//question 4 Create a variable called division and initialise it with a value of 20 divided by 5.
+//answer 4
+var division = 20 / 5;
+//question 5 Write code that checks the type of the value "frog".
+//answer 5
+typeof "frog"
+//question 6 Declare and initialise a variable called orderHasShipped with a boolean value.
+//answer 6
+var orderHasShipped = true;
+//question 7 Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+//answer 7
+var orderHasShipped;
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+//question 8 Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+//answer 8
+for (var i = 0; i<10; i += 1) {
+	console.log(i);
+}
